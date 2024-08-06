3c1a978ae450d0457a7e0ec2c180daa7b575576e
hibiki <hibiki1023k@gmail.com>
2024-08-06 19:25:41 +0900

    feat: setup for yamada-ui

---
+++ blog-client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from '@yamada-ui/react'
+
+function App() {
+
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default App
